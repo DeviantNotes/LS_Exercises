e344da60199457fa5de1c4aad2159b4bb84ca15b
Sam Ralph <samsralph@icloud.com>
2023-02-10 15:06:26 -0500

    Adding exercises

---
+++ RB101-RB109-Small_Problems/Easy_3/palindromic_numbers.rb
@@ -0,0 +1,16 @@
+def palindromic_number?(number, number_of_leading_zeroes = 0)
+  if number_of_leading_zeroes > 0
+    leading_zeroes = ''
+    number_of_leading_zeroes.times { leading_zeroes << '0' }
+    number = "#{leading_zeroes}%o" % number
+  else
+    number = number.to_s
+  end
+  number == number.reverse
+end
+
+puts palindromic_number?(001232100, 2) == true
+puts palindromic_number?(34543) == true
+puts palindromic_number?(123210) == false
+puts palindromic_number?(22) == true
+puts palindromic_number?(5) == true
--- RB101-RB109-Small_Problems/Easy_3/palindromic_strings2.rb
+++ RB101-RB109-Small_Problems/Easy_3/palindromic_strings2.rb
@@ -1,16 +1,14 @@
 require 'pry'
 
 def real_palindrome?(string)
-  string.downcase!
-  new_string = string.downcase.delete(" ", "'", ",")
-  # string.delete!(" ")
-  # string.slice!(/[[:alnum:]]/)
-  binding.pry
-  new_string == string.reverse
+  arr = string.downcase.scan(/\w*/)
+  new_string = ''
+  arr.each { |word| new_string << word }
+  new_string == new_string.reverse
 end
 
-# puts real_palindrome?('madam') == true
-# puts real_palindrome?('Madam') == true           # (case does not matter)
+puts real_palindrome?('madam') == true
+puts real_palindrome?('Madam') == true           # (case does not matter)
 puts real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
 puts real_palindrome?('356653') == true
 puts real_palindrome?('356a653') == true
